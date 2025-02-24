0c8b33ca9f3b131cad0f0bfa4a0a2b82e5874f7b
Ashish Baravaliya <vier.institute35@gmail.com>
2025-02-24 12:40:26 -0500

    fix: customer_has_balance api error issue

--- d2h/api.py
+++ d2h/api.py
@@ -1,4 +1,4 @@
-from apps.erpnext.erpnext.accounts.party import get_dashboard_info
+from erpnext.erpnext.accounts.party import get_dashboard_info
 import frappe
 from frappe.model.mapper import get_mapped_doc
 from frappe.utils import today
